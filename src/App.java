3e0f296374b4012a9ae1d846cbd345cdb2493267
Victor Torres <victor.felipe_torres@hotmail.com>
2024-04-15 20:58:08 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Você se chama Nickira, e está em uma trilha pela floresta, porém você perde a hora e fica de noite de repente com uma grande chuva, você deve ir em busca de alguma casa para pedir abrigo... ");
+        System.out.println("\n Você encontra uma fazenda e toca a campainha, um senhorzinho atende e chama você para entrar. Qual sua decisão?");
+        System.out.println("\n Entrar na casa(Entrar), Recusar e ficar do lado de fora(Recusar) ou Ir para o celeiro escondido(Celeiro).");
+
+        String resp1 = input.nextLine();
+
+        if (resp1.toUpperCase().equals("ENTRAR")) {
+            System.out.println("Você adentra a casa.");
+        }
+        else if (resp1.toUpperCase().equals("RECUSAR")) {
+            System.out.println("Você recusa e fica na frente da casa.");
+        }
+        else if (resp1.toUpperCase().equals("CELEIRO")) {
+            System.out.println("Você vai de fininho até o celeiro e acha um jeito de entrar.");
+        }
+        else {
+            System.out.println("Resposta inválida, tente novamente.");
+        }
+        input.close();
+    }
+}
